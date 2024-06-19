ecaf4777aff1a974278f796619a40b8e1ed6b08f
tonymushah <tonyramanantsalama@gmail.com>
2024-06-19 15:47:44 +0300

    Added second

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use std::{fs::File, io::BufWriter};
+use std::{fs::File, io::{BufReader, BufWriter}};
 
-use tar::Builder;
+use tar::{Archive, Builder};
 
-fn main() {
+fn first() {
     let mut some = File::open("some.txt").unwrap();
     let mut arch_builder = Builder::new(BufWriter::new(File::create("some.tar").unwrap()));
     arch_builder
         .append_file("texts/some.txt", &mut some)
         .unwrap();
     arch_builder.finish().unwrap();
+}
+
+fn second() {
+    let some = BufReader::new(File::open("some.tar").unwrap());
+    let mut arch = Archive::new(some);
+    for entry in arch.entries().unwrap().flatten() {
+        println!("{}", entry.path().unwrap().to_path_buf().to_str().unwrap());
+    }
+}
+
+fn main() {
+    second();
     println!("Hello, world!");
 }
