24f8bb2b424441fea6e827ad6aa702f6d4ed88e4
tonymushah <tonyramanantsalama@gmail.com>
2024-06-19 15:41:18 +0300

    Added first tar update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use std::{fs::File, io::BufWriter};
+
+use tar::Builder;
+
 fn main() {
+    let mut some = File::open("some.txt").unwrap();
+    let mut arch_builder = Builder::new(BufWriter::new(File::create("some.tar").unwrap()));
+    arch_builder
+        .append_file("texts/some.txt", &mut some)
+        .unwrap();
+    arch_builder.finish().unwrap();
     println!("Hello, world!");
 }
-
