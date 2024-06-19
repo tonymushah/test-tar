2e98243dcf3a8db1adefd941ea64b7a8ba8f6723
tonymushah <tonyramanantsalama@gmail.com>
2024-06-19 15:52:09 +0300

    Archiving with ztd

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::{fs::File, io::{BufReader, BufWriter}};
 
 use tar::{Archive, Builder};
+use zstd::{Decoder, Encoder};
 
 fn first() {
     let mut some = File::open("some.txt").unwrap();
-    let mut arch_builder = Builder::new(BufWriter::new(File::create("some.tar").unwrap()));
+    let mut arch_builder = Builder::new(Encoder::new(BufWriter::new(File::create("some.tar.ztd").unwrap()), 19).unwrap().auto_finish());
     arch_builder
         .append_file("texts/some.txt", &mut some)
         .unwrap();
@@ -12,7 +13,7 @@ fn first() {
 }
 
 fn second() {
-    let some = BufReader::new(File::open("some.tar").unwrap());
+    let some = Decoder::new(BufReader::new(File::open("some.tar.ztd").unwrap())).unwrap();
     let mut arch = Archive::new(some);
     for entry in arch.entries().unwrap().flatten() {
         println!("{}", entry.path().unwrap().to_path_buf().to_str().unwrap());
@@ -20,6 +21,7 @@ fn second() {
 }
 
 fn main() {
+    first();
     second();
     println!("Hello, world!");
 }
